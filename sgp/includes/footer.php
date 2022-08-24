0564b8b73db8262f0222facafd955ed0223826cd
Kunal <kunalsheth1177@gmail.com>
2022-08-24 13:20:27 +0530

    Inital Commit

---
+++ sgp/includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+
+                        </div>
+                    </div>
+                </footer>
+            </div>
